5a1a4f33cb13b6ea6bc90bbefb6f512e9b08bd24
Seth Lachman <lachmanseth17@gmail.com>
2019-11-25 13:49:41 -0500

    Added the get and drop methods to player.

--- src/adv.py
+++ src/adv.py
@@ -43,7 +43,7 @@ room['treasure'].s_to = room['narrow']
 player = Player(room['outside'], [])
 
 
-def listToString(items):
+def list_to_string(items):
     new_str = ""
 
     for item in items:
@@ -57,7 +57,7 @@ while end != True:
     wrapper = textwrap.TextWrapper(width=50)
 
     description = wrapper.wrap(
-        text=player.room.description + "\n               " + "Items in room: " + listToString(player.room.items))
+        text=player.room.description + "\n               " + "Items in room: " + list_to_string(player.room.items))
 
     for word in description:
         print(word)
@@ -82,9 +82,33 @@ while end != True:
             player.room = player.room.w_to
         else:
             print("Not allowed to move there")
+    if user_input == "i":
+        print(list_to_string(player.room.items))
+        item_user_input = input(
+            "\nWould you like to pick up an item from this room? Or drop off an item in this room?\n")
+
+        if item_user_input == "d":
+            if not player.items:
+                print("You don't have any items to drop off")
+            else:
+                item_to_drop = input("Which item would you like to drop off?")
+                if item_to_drop in player.items:
+                    player.room.items.append(item_to_drop)
+                    player.items.remove(item_to_drop)
+                    print(item_to_drop + " has been dropped in " +
+                          player.room.name)
+            # logic for dropping item
+        elif item_user_input == "a":
+            item_to_add = input("Which item would you like to add?")
+            if (item_to_add in player.room.items):
+                player.items.append(item_to_add)
+                player.room.items.remove(item_to_add)
+                print(item_to_add + " has been added from" +
+                      player.room.name)
+            # logic for adding item
     if user_input == "q":
         end = True
-    print(user_input)
+
 # Write a loop that:
 #
 # * Prints the current room name
