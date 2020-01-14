12efaf7420db6c52541d0c8fd1fb8b82343812e2
Seth Lachman <lachmanseth17@gmail.com>
2020-01-14 09:52:45 -0500

    Added item class.

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,26 @@
 import textwrap
 from room import Room
 from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", ["candlestick", "cigar", "match"]),
+                     "North of you, the cave mount beckons", [Item("candlestick", "it helps you find your way in the dark"), Item("cigar", "smoking is fun"), Item("match", "used for smoking")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", ["candlestick", "cigar", "match"]),
+passages run north and east.""", [Item("coin", "Gold and shiny"), Item("sword", "used in war"), Item("wine", "red and white")]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", ["candlestick", "cigar", "match"]),
+the distance, but there is no way across the chasm.""", [Item("goblet", "for wine"), Item("knife", "sneaky"), Item("shield", "defense")]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", ["candlestick", "cigar", "match"]),
+to north. The smell of gold permeates the air.""", [Item("poison", "sneakier"), Item("lamp", "lamp lamps"), Item("pipe", "More tobacco")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", ["candlestick", "cigar", "match"]),
+earlier adventurers. The only exit is to the south.""", [Item("pie", "cherry pie"), Item("wood", "burning wood"), Item("saw", "used for wood")]),
 }
 
 
@@ -54,58 +55,63 @@ def list_to_string(items):
 end = False
 while end != True:
     print(player.room.name)
-    wrapper = textwrap.TextWrapper(width=50)
+    wrapper = textwrap.TextWrapper(width=70)
 
     description = wrapper.wrap(
-        text=player.room.description + "\n               " + "Items in room: " + list_to_string(player.room.items))
+        text=player.room.description + "\n")
+
+    print("Items in room: ")
+
+    for item in player.room.items:
+        print(f"{item.name}")
 
     for word in description:
         print(word)
     user_input = input("Choose a room: ")
+    first_word = user_input.split()[0]
+    second_word = user_input.split()[1]
+
     if user_input == "n":
         if hasattr(player.room, "n_to"):
             player.room = player.room.n_to
         else:
-            print("Not allowed to move there")
+            print("You are not allowed to move in that direction")
     if user_input == "s":
         if hasattr(player.room, "s_to"):
             player.room = player.room.s_to
         else:
-            print("Not allowed to move there")
+            print("You are not allowed to move in that direction")
     if user_input == "e":
         if hasattr(player.room, "e_to"):
             player.room = player.room.e_to
         else:
-            print("Not allowed to move there")
+            print("You are not allowed to move in that direction")
     if user_input == "w":
         if hasattr(player.room, "w_to"):
             player.room = player.room.w_to
         else:
-            print("Not allowed to move there")
-    if user_input == "i":
-        print(list_to_string(player.room.items))
-        item_user_input = input(
-            "\nWould you like to pick up an item from this room? Or drop off an item in this room?\n")
-
-        if item_user_input == "d":
-            if not player.items:
-                print("You don't have any items to drop off")
-            else:
-                item_to_drop = input("Which item would you like to drop off?")
-                if item_to_drop in player.items:
-                    player.room.items.append(item_to_drop)
-                    player.items.remove(item_to_drop)
-                    print(item_to_drop + " has been dropped in " +
-                          player.room.name)
-            # logic for dropping item
-        elif item_user_input == "a":
-            item_to_add = input("Which item would you like to add?")
-            if (item_to_add in player.room.items):
-                player.items.append(item_to_add)
-                player.room.items.remove(item_to_add)
-                print(item_to_add + " has been added from" +
+            print("You are not allowed to move in that direction")
+    if first_word == "drop":
+        if not player.items:
+            print("You don't have any items to drop off")
+        else:
+            if item_to_drop in player.items:
+                player.room.items.append(item_to_drop)
+                player.items.remove(item_to_drop)
+                print(item_to_drop + " has been dropped in " +
                       player.room.name)
-            # logic for adding item
+        # logic for dropping item
+    elif first_word == "get" or first_word == "take":
+
+        if (second_word in player.room.items):
+            player.add_item(second_word)
+            player.room.items.remove(second_word)
+            print(second_word + " has been added from " +
+                  player.room.name)
+    if user_input == "i":
+        print(list_to_string(player.items))
+
+        # logic for adding item
     if user_input == "q":
         end = True
 
--- src/player.py
+++ src/player.py
@@ -6,3 +6,12 @@ class Player():
     def __init__(self, room, items):
         self.room = room
         self.items = items
+        self.item_names = [item.name for item in self.items]
+
+    def add_item(self, item):
+        self.items.append(item)
+        item.on_take()
+
+    def remove_item(self, item):
+        self.items.remove(item)
+        itm.on_drop()
